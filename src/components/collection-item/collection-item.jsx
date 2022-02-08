bbe3ffb47b9c8aa9ba3df6182fd080b1a94d42d5
mdluc <madalinaluceac@gmail.com>
2022-02-08 18:11:11 +0200

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import "./collection-item.scss"
+import CustomButton from "../custom-button/custom-button";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cart-actions";
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
+const CollectionItem = ({item, addItem}) =>{
+const {imageUrl, name, price} = item;
+    return(
+        <div className="collection-item">
+            <div 
+            className="image"
+            style={{backgroundImage:`url(${imageUrl})`}}
+            />
+            <div className="collection-footer">
+                <span className="name">{name}</span>
+                <span className="price">{price}</span>
+            </div>
+            <CustomButton inverted onClick={()=>addItem(item)}>Add to cart</CustomButton>
+        </div>    
+    )
+}
 
-<div className="collection-item">
-    <div 
-    className="image"
-    style={{backgroundImage:`url(${imageUrl})`}}
-    />
-    <div className="collection-footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-    </div>
-    
-</div>    )
+const mapDispatchToProps = (dispatch) =>({
+    addItem: item => dispatch(addItem(item))
+})
 
-export default CollectionItem;
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/custom-button/custom-button.jsx
+++ src/components/custom-button/custom-button.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import "./custom-button.scss"
+import React from "react";
+import "./custom-button.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-    <button 
-    className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`} 
-    {...otherProps}>
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
+  <button
+    className={`${inverted ? "inverted" : ""} 
+    ${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+    {...otherProps}
+  >
     {children}
-    </button>
+  </button>
 );
-
 
 export default CustomButton;
